aef600b753229934a89d96f40ae241a9f9133a94
JayWll <jaywll@outlook.com>
2020-02-18 19:47:27 -0700

    Removing tests, incorporating check for holidays into main function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fetch = require('fetch');
+const fetch = require('node-fetch');
 const https = require('https');
 const express = require('express');
 const app = express();
@@ -7,63 +7,65 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/test', (req, res) => {
-  var now = new Date();
-  isaholiday(now.toISOString().substr(0, 10), (holiday) => {
-    res.send(holiday);
-  })
-})
 
 app.get('/bot/trigger', (req, res) => {
+  // Check that the expected key has been included with the web request
   if (!req.headers['moodmarbles-key'] || req.headers['moodmarbles-key'] != process.env.AUTH_KEY) {
     return res.status(401).send('Authorization header not found').end();
   }
 
-  var now = new Date();
-  var message = {};
+  // Check if today is a holiday
+  const now = new Date();
+  isaholiday(now.toISOString().substr(0, 10), (holiday) => {
+    // If today is a holiday, a Sunday or a Saturday, stop the request.
+    if (holiday || now.getDay() < 1 || now.getDay() > 5) return res.status(200).send("No message today").end();
 
-  switch(now.getDay()) {
-    case 1:
-      message.text = "It's a brand new week, <users/all>! How's everyone feeling on this Monday morning? Hover over this message and click “add reaction.”";
-      break;
-    case 2:
-      message.text =  "Morning <users/all>, and happy Tuesday. How's everyone feeling today? Hover over this message and click “add reaction.”";
-      break;
-    case 3:
-      message.text = "Happy hump day <users/all>. We're midway through the workweek already. How's everybody feeling? Hover over this message and click “add reaction.”";
-      break;
-    case 4:
-      message.text = "Hey <users/all>, it's the <https://www.youtube.com/watch?v=uVPKmeB8p94|third day> and the weekend is in sight. How's everybody feeling this morning? Hover over this message and click “add reaction.”";
-      break;
-    case 5:
-      message.text = "Happy Friday, <users/all>! How's everyone feeling today as we wrap up the week? Hover over this message and click “add reaction.”";
-      break;
-  }
+    // Build today's message
+    var message = {};
 
-  var options = {
-    host: 'chat.googleapis.com',
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
+    switch(now.getDay()) {
+      case 1:
+        message.text = "It's a brand new week, <users/all>! How's everyone feeling on this Monday morning? Hover over this message and click “add reaction.”";
+        break;
+      case 2:
+        message.text =  "Morning <users/all>, and happy Tuesday. How's everyone feeling today? Hover over this message and click “add reaction.”";
+        break;
+      case 3:
+        message.text = "Happy hump day <users/all>. We're midway through the workweek already. How's everybody feeling? Hover over this message and click “add reaction.”";
+        break;
+      case 4:
+        message.text = "Hey <users/all>, it's the <https://www.youtube.com/watch?v=uVPKmeB8p94|third day> and the weekend is in sight. How's everybody feeling this morning? Hover over this message and click “add reaction.”";
+        break;
+      case 5:
+        message.text = "Happy Friday, <users/all>! How's everyone feeling today as we wrap up the week? Hover over this message and click “add reaction.”";
+        break;
     }
-  };
 
-  if (message.text) {
+    // Define the options for the https request
+    var options = {
+      host: 'chat.googleapis.com',
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
+
+    // Send the message to AUTO_TEAM_URL
     options.path = process.env.AUTO_TEAM_URL;
     sendRequest(options, message);
 
+    // Send the message to FED_TEAM_URL
     options.path = process.env.FED_TEAM_URL;
     sendRequest(options, message);
-  }
 
-  res
-    .status(200)
-    .send('OK')
-    .end();
+    // Return a response to the web request
+    res.status(200).send('OK').end();
+  });
 });
 
 // Function to send a HTTP request
 const sendRequest = (opt, msg) => {
+  var https = require("https")
   var request = https.request(opt, function(response) {
     var responseString = "";
 
@@ -86,7 +88,7 @@ const isaholiday = (isodate, callback) => {
 
   fetch(url).then((res) => res.json()).then((json) => {
     for(var i = 0; i < json.response.holidays.length; i++) {
-      if (json.response.holidays[i].date.iso == isodate) callback(true);
+      if (json.response.holidays[i].date.iso == isodate) return callback(true);
     }
 
     callback(false);
